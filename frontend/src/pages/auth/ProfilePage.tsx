cfa964c6dfea904fd70a21814f59384595ad7b86
PURNABRATA DEY <deypurnabrata2005@gmail.com>
2026-05-14 20:49:29 +0530

    feat: refactor ProfilePage component for improved structure and styling

--- frontend/src/pages/auth/ProfilePage.tsx
+++ frontend/src/pages/auth/ProfilePage.tsx
@@ -1,11 +1,39 @@
+import HeatmapCalendar from "@/components/profile/HeatmapCalendar";
+import ProfileHeader from "@/components/profile/ProfileHeader";
+import ProfileTabSubmissions from "@/components/profile/ProfileTabSubmissions";
+import { useAuthStore, useProblemStore, useSubmissionStore } from "@/store";
+import { useEffect } from "react";
+export default function ProfilePage() {
+  const { authUser: user } = useAuthStore();
 
-
-const ProfilePage = () => {
+  const {
+    getSubmissionStats,
+    submissionStats,
+    isSubmissionStatsLoading,
+    getHeatmapData,
+    hetmapData,
+    isHetmapLoading,
+  } = useSubmissionStore();
+  const { getUserSolvedProblemsRank, userRank } = useProblemStore();
+  useEffect(() => {
+    getSubmissionStats();
+    getHeatmapData();
+    getUserSolvedProblemsRank(user?.id || "");
+  }, [getSubmissionStats, getHeatmapData, getUserSolvedProblemsRank, user?.id]);
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="bg-background mx-auto min-h-screen max-w-7xl">
+      <ProfileHeader
+        isLoading={isSubmissionStatsLoading}
+        submissionStats={submissionStats}
+        userData={user}
+      />
 
-export default ProfilePage
+      <HeatmapCalendar
+        isLoading={isHetmapLoading}
+        data={hetmapData}
+        userRank={userRank}
+      />
+      <ProfileTabSubmissions />
+    </div>
+  );
+}
